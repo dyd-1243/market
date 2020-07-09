a95b1977dc8b6ca475c7429caa38da5c0e5d50b6
dyd-1243 <1243775176@qq.com>
2020-07-09 16:54:41 +0800

    init rep

---
+++ src/test/java/lift/dd/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package lift.dd.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
